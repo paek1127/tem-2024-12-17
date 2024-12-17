94bc917272f0d5ff973e0686e328413a55de4051
paek1127 <paek1127@gmail.com>
2024-12-17 10:45:26 +0900

    post 주소 write로 변경

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -14,7 +14,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="/posts/doWrite" method="post">
+                <form action="/posts/write" method="post">
                     <input type="text" name="title" placeholder="제목">
                     <textarea name="content" placeholder="내용"></textarea>
                     <button type="submit">글쓰기</button>
@@ -22,7 +22,7 @@ public class PostController {
                 """;
     }
 
-    @PostMapping("/doWrite")
+    @PostMapping("/write")
     @ResponseBody
     public String write(
             String title,
